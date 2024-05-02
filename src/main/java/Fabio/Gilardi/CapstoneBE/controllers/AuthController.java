a1f164ab9ec970dbfb8c87f98ebb719137dfc536
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-02 16:26:26 +0200

    AuthController added

---
+++ src/main/java/Fabio/Gilardi/CapstoneBE/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package Fabio.Gilardi.CapstoneBE.controllers;
+
+import Fabio.Gilardi.CapstoneBE.entities.User;
+import Fabio.Gilardi.CapstoneBE.exceptions.BadRequestException;
+import Fabio.Gilardi.CapstoneBE.payloads.LoginResponseDTO;
+import Fabio.Gilardi.CapstoneBE.payloads.NewUserDTO;
+import Fabio.Gilardi.CapstoneBE.payloads.UserLoginDTO;
+import Fabio.Gilardi.CapstoneBE.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody @Validated NewUserDTO payload,
+                         BindingResult validation) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        return this.authService.register(payload);
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody @Validated UserLoginDTO payload,
+                                  BindingResult validation) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        return new LoginResponseDTO(this.authService.login(payload));
+    }
+
+}
