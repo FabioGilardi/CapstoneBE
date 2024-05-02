29950d8447d4950524b90c59cfe578d080c7ebe8
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-02 16:19:11 +0200

    User class added

---
+++ src/main/java/Fabio/Gilardi/CapstoneBE/entities/User.java
@@ -0,0 +1,83 @@
+package Fabio.Gilardi.CapstoneBE.entities;
+
+import Fabio.Gilardi.CapstoneBE.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties({"password", "role", "authorities", "accountNonExpired", "accountNonLocked", "credentialsNonExpired", "enabled"})
+public class User implements UserDetails {
+
+    //    ATTRIBUTES
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+    private String avatar, username, email, password, name, surname;
+    private LocalDate birthDate;
+    private UserRole role;
+
+    //    CONSTRUCTORS
+    public User(String username, String email, String password, String name, String surname, LocalDate birthDate) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.role = UserRole.USER;
+        setDefaultAvatar();
+    }
+
+    //    METHODS
+    public void setDefaultAvatar() {
+        this.avatar = "https://ui-avatars.com/api/?name=" + this.name + "+" + this.surname;
+    }
+
+    public long getAge() {
+        return ChronoUnit.YEARS.between(this.birthDate, LocalDate.now());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
