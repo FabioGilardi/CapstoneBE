1884b00d1f4529c37689d317bbf342091923c6dd
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-03 17:38:53 +0200

    AccessoryController added + tested(Postman)

---
+++ src/main/java/Fabio/Gilardi/CapstoneBE/controllers/AccessoryController.java
@@ -0,0 +1,20 @@
+package Fabio.Gilardi.CapstoneBE.controllers;
+
+import Fabio.Gilardi.CapstoneBE.services.AccessoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accessories")
+public class AccessoryController {
+
+    @Autowired
+    private AccessoryService accessoryService;
+
+    @PostMapping("/populate")
+    public void populateDB() {
+        this.accessoryService.populateDB();
+    }
+}
