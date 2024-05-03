242e281c58b395f29de9b878d16b638456bba063
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-03 12:08:26 +0200

    NewReviewDTO added

---
+++ src/main/java/Fabio/Gilardi/CapstoneBE/payloads/NewReviewDTO.java
@@ -0,0 +1,22 @@
+package Fabio.Gilardi.CapstoneBE.payloads;
+
+import jakarta.validation.constraints.*;
+
+public record NewReviewDTO(
+        @NotEmpty(message = "title is mandatory")
+        @Size(min = 4, message = "title must be longer than 3 characters")
+        String title,
+
+        @NotEmpty(message = "description is mandatory")
+        String description,
+
+        @NotNull(message = "rating is mandatory")
+        @Min(value = 0, message = "rating can't be less than 0")
+        @Max(value = 5, message = "rating can't be more than 5")
+        int rating,
+
+        @NotNull(message = "user id is mandatory")
+        @Min(value = 0, message = "user id must be more than 0")
+        long userId
+) {
+}
