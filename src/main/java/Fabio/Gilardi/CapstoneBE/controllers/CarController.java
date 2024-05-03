d1a1a3bcd7be6f2b28c9256c6f985a32652dd654
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-03 16:01:28 +0200

    CarController added + tested (Postman)

---
+++ src/main/java/Fabio/Gilardi/CapstoneBE/controllers/CarController.java
@@ -0,0 +1,60 @@
+package Fabio.Gilardi.CapstoneBE.controllers;
+
+import Fabio.Gilardi.CapstoneBE.entities.Car;
+import Fabio.Gilardi.CapstoneBE.exceptions.BadRequestException;
+import Fabio.Gilardi.CapstoneBE.payloads.NewCarDTO;
+import Fabio.Gilardi.CapstoneBE.payloads.UpdateCarDTO;
+import Fabio.Gilardi.CapstoneBE.services.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    CarService carService;
+
+    @GetMapping
+    public Page<Car> findAll(@RequestParam(defaultValue = "0") int number,
+                             @RequestParam(defaultValue = "50") int size,
+                             @RequestParam(defaultValue = "id") String sortBy) {
+        return this.carService.findAll(number, size, sortBy);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAuthority('SELLER')")
+    public Car save(@RequestBody @Validated NewCarDTO payload,
+                    BindingResult validation) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        return this.carService.save(payload);
+    }
+
+    @GetMapping("/{id}")
+    public Car findById(@PathVariable long id) {
+        return this.carService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('SELLER')")
+    public Car findByIdAndUpdate(@PathVariable long id,
+                                 @RequestBody @Validated UpdateCarDTO payload,
+                                 BindingResult validation
+    ) {
+        if (validation.hasErrors()) throw new BadRequestException(validation.getAllErrors());
+        return this.carService.findByIdAndUpdate(id, payload);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('SELLER')")
+    public void findByIdAndDelete(@PathVariable long id) {
+        this.carService.findByIdAndDelete(id);
+    }
+}
