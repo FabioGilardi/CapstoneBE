dbab84d40a895f2a83c18c44e1e0af42a034942a
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-03 17:37:24 +0200

    Accessory class + AccessoryName fixed

--- src/main/java/Fabio/Gilardi/CapstoneBE/entities/Accessory.java
+++ src/main/java/Fabio/Gilardi/CapstoneBE/entities/Accessory.java
@@ -1,10 +1,7 @@
 package Fabio.Gilardi.CapstoneBE.entities;
 
 import Fabio.Gilardi.CapstoneBE.enums.AccessoryName;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +19,7 @@ public class Accessory {
     @Setter(AccessLevel.NONE)
     private long id;
 
+    @Enumerated(EnumType.STRING)
     private AccessoryName accessoryName;
 
     //    CONSTRUCTORS
