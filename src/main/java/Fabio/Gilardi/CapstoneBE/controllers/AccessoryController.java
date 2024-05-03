1f9d488e37d6cc89d0428ce5acc5a0246f66b402
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-05-03 17:41:46 +0200

    AccessoryController fixed with ADMIN authorization

--- src/main/java/Fabio/Gilardi/CapstoneBE/controllers/AccessoryController.java
+++ src/main/java/Fabio/Gilardi/CapstoneBE/controllers/AccessoryController.java
@@ -2,6 +2,7 @@ package Fabio.Gilardi.CapstoneBE.controllers;
 
 import Fabio.Gilardi.CapstoneBE.services.AccessoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class AccessoryController {
     private AccessoryService accessoryService;
 
     @PostMapping("/populate")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void populateDB() {
         this.accessoryService.populateDB();
     }
